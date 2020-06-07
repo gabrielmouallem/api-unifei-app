4f328c3083c1e5111fce12b77f3f1ca40886450d
Gabriel José Mouallem Rodrigues <40147856+gabrielmouallem@users.noreply.github.com>
2020-06-07 16:49:17 -0300

    Marker models registered on django admin site.

--- markers/admin.py
+++ markers/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from markers.models import GenericMarker, EventMarker, ConstructionMarker, StudyGroupMarker, ExtraActivityMarker
+
+admin.site.register(GenericMarker)
+admin.site.register(EventMarker)
+admin.site.register(ConstructionMarker)
+admin.site.register(StudyGroupMarker)
+admin.site.register(ExtraActivityMarker)
