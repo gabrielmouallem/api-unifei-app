8ec3152d08e5a9d486907189ece61e0d0883f5da
Gabriel Mouallem <gabriel.unifei2017@gmail.com>
2020-11-22 18:29:26 -0300

    Marker to classrom route implemented

--- schedules/views.py
+++ schedules/views.py
@@ -11,29 +11,33 @@ from rest_framework.response import Response
 from schedules.serializers import ScheduleSerializer
 
 
-class RetriveScheduleViewByProfile(APIView):
-    permission_classes = (IsAuthenticated, )
-    authentication_classes = (TokenAuthentication, )
-    def get(self, request, *args, **kwargs):
-        schedule = Schedule.objects.filter(profile=Profile.objects.get(user_id=self.request.user.id))
-        if schedule.exists():
-            return Response(data=ScheduleSerializer(instance=schedule.first()).data, status=200)
-        else:
-            return Response(status=200, data=[])
-
-
 class AddMarkerToClassroomView(APIView):
     permission_classes = (IsAuthenticated, )
     authentication_classes = (TokenAuthentication, )
 
     def post(self, request, *args, **kwargs):
-        if self.request.data['classroom_id'] is not None and self.request.data['marker_id']:
-            classroom = Classroom.objects.filter(id=self.request.data['classroom_id'])
-            if classroom.exists():
-                classroom.marker = Marker.objects.get(id=self.request.data['marker_id'])
+        if self.request.data['classroom_id'] is not None and self.request.data['marker_id'] is not None:
+            marker_query = Marker.objects.filter(id=self.request.data['marker_id'])
+            classroom_query = Classroom.objects.filter(id=self.request.data['classroom_id'])
+            if marker_query.exists() and classroom_query.exists():
+                marker = marker_query.first()
+                classroom = classroom_query.first()
+                classroom.marker = marker
                 classroom.save()
                 return Response(status=200)
             else:
                 return Response(status=400)
         else:
             return Response(status=400)
+
+
+class RetriveScheduleViewByProfile(APIView):
+    permission_classes = (IsAuthenticated, )
+    authentication_classes = (TokenAuthentication, )
+
+    def get(self, request, *args, **kwargs):
+        schedule = Schedule.objects.filter(profile=Profile.objects.get(user_id=self.request.user.id))
+        if schedule.exists():
+            return Response(data=ScheduleSerializer(instance=schedule.first()).data, status=200)
+        else:
+            return Response(status=200, data=[])
